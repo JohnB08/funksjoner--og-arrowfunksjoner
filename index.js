6319b0661b8657e22e9c46748c64e623e774316b
JohnB08 <johnb08.kodehode@gmail.com>
2023-10-02 11:24:35 +0200

    better button select

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 let answer = document.getElementById("desc");
 let numCon1 = document.getElementById("num1");
 let numCon2 = document.getElementById("num2");
-let btnAdd = document.getElementById("btn1");
-let btnSub = document.getElementById("btn2");
-let btnMult = document.getElementById("btn3");
-let btnDivide = document.getElementById("btn4");
+let btn = document.querySelectorAll("button");
 
 function add(num1, num2) {
   return num1 + num2;
@@ -12,25 +9,25 @@ function add(num1, num2) {
 function subtract(num1, num2) {
   return num1 - num2;
 }
-function multiply(num1, num2) {
+const multiply = (num1, num2) => {
   return num1 * num2;
-}
+};
 const divide = (num1, num2) => {
   return num1 / num2;
 };
-btnAdd.addEventListener("click", () => {
+btn[0].addEventListener("click", () => {
   let sum = add(numCon1.valueAsNumber, numCon2.valueAsNumber);
   answer.textContent = `Your answer is ${sum}.`;
 });
-btnSub.addEventListener("click", () => {
+btn[1].addEventListener("click", () => {
   let sum = subtract(numCon1.valueAsNumber, numCon2.valueAsNumber);
   answer.textContent = `Your answer is ${sum}.`;
 });
-btnMult.addEventListener("click", () => {
+btn[2].addEventListener("click", () => {
   let sum = multiply(numCon1.valueAsNumber, numCon2.valueAsNumber);
   answer.textContent = `Your answer is ${sum}.`;
 });
-btnDivide.addEventListener("click", () => {
+btn[3].addEventListener("click", () => {
   let sum = divide(numCon1.valueAsNumber, numCon2.valueAsNumber);
   answer.textContent = `Your answer is ${sum}.`;
 });
